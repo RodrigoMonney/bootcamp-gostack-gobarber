334dfe900e5c5c40fd55244f35b8797ebe76d6f5
Rodrigo Reis <rodrigo.monney@gmail.com>
2019-11-13 23:56:13 -0300

    chore: configure eslint, prettier and editorConfig for code standardization

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-import { Router } from 'express'
+import { Router } from 'express';
 
-const routes = new Router()
+const routes = new Router();
 
 routes.get('/', (req, res) => {
-  return res.json({ message: "I will work at EBANX!" })
-})
+  return res.json({ message: 'I will work at EBANX!' });
+});
 
 export default routes;
