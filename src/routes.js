9144e238aa07af0ca17a0ad885876c44f3bafa1a
Rodrigo Reis <rodrigo.monney@gmail.com>
2019-12-16 21:39:55 -0300

    chore: add bcryptjs to hash passwords

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'I will work at EBANX!' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Rodrigo Monney',
+    email: 'rodrigo.monney@gmail.com',
+    password_hash: 'senha123',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
