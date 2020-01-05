fa2e1fe3a7d362694275d8e2b493700bcc00f30e
Rodrigo Reis <rodrigo.monney@gmail.com>
2020-01-05 20:57:28 -0300

    chore: create rule to notify an appointment to the provider

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,10 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -49,6 +51,15 @@ class AppointmentController {
     const { date, provider_id } = req.body;
 
     /**
+     * Check if userId is equal than providerId
+     */
+    if (req.userId === provider_id) {
+      return res
+        .status(401)
+        .json({ error: 'User has to be different than provider.' });
+    }
+
+    /**
      * Check if a provider_id is a provider
      */
     const isProvider = await User.findOne({
@@ -87,6 +98,21 @@ class AppointmentController {
       user_id: req.userId,
       provider_id,
       date: hourStart,
+    });
+
+    /**
+     * Notify appointment provider
+     */
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' H:mm'h'",
+      { locale: pt }
+    );
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para ${formattedDate}`,
+      user: provider_id,
     });
 
     return res.json(appointment);
